571e130f30ce8582c038056f4a5cda87b21f76e2
KurrybroX <kuribohx@gmail.com>
2025-02-09 18:08:55 -0500

    created screen, added close functionality, ignored venv & pycache

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pygame
-from database import connect_database, database_version
-
+from constants import *
 def main():
   print("Starting asteroids!")
+  print(f"Screen width: {SCREEN_WIDTH}")
+  print(f"Screen height: {SCREEN_HEIGHT}")
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        return
+    screen.fill(000000)
+    pygame.display.flip()
+
+  
 
 if __name__ == "__main__":
   main()
